2f18f6e4d4503a22a9dc4ed39855c1822b72ad79
Julian Finelli <julianfinelli97@gmail.com>
2024-02-24 18:12:38 -0300

    chore: ultimos cambios proyecto

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,9 @@
-import "./_projectsMobile.scss";
-import "./_projectsIpad.scss";
 import "./_projectsDesktop.scss"
+import "./_projectsIpad.scss"
+import "./_projectsMobile.scss"
+import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { useState } from "react";
 import ProyectCard from "../ProjectCard/ProyectCard";
 import { arrayProjectsPage } from "../../data/arrayProyectsPage";
 import ReactTag from "../Tags/ReactTag/ReactTag";
@@ -18,17 +18,22 @@ import JavascriptTag from "../Tags/JavascriptTag/JavascriptTag"
 import HtmlTag from "../Tags/HtmlTag/HtmlTag"
 import CssTag from "../Tags/CssTag/CssTag"
 import FireBaseTag from "../Tags/FirebaseTag/FireBaseTag"
+import "./_projectsDesktop.scss";
+import "./_projectsIpad.scss";
+import "./_projectsMobile.scss";
 
 export default function Projects() {
     const [searchTerm, setSearchTerm] = useState('');
     const [filter, setFilter] = useState('All');
+    const [showMore, setShowMore] = useState(false);
+    const [projectsToShow, setProjectsToShow] = useState(6); // Mostrar inicialmente 6 proyectos
 
-    // Función para manejar el cambio de filtro
     const handleFilterChange = (selectedFilter) => {
         setFilter(selectedFilter);
+        setShowMore(true)
+        setProjectsToShow(projectsToShow + 3); // Reiniciar el contador de proyectos mostrados al cambiar el filtro
     };
 
-    // Función para manejar el cambio en el valor del input de búsqueda
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
     };
@@ -48,6 +53,11 @@ export default function Projects() {
         CssTag
     };
 
+    const handleShowMore = () => {
+        setShowMore(true);
+        setProjectsToShow(projectsToShow + 3); // Mostrar otros 3 proyectos adicionales
+    };
+
     return (
         <article id="projects" className="contenedorGralProjects">
             <div className="contenedorTitulo">
@@ -57,68 +67,76 @@ export default function Projects() {
                 <h4>Proyectos Destacados</h4>
             </div>
             <div className="contenedorBuscador">
-              <div className="internoBuscador">
-                <div className='buscador'>
-                  <FontAwesomeIcon className='iconLupa' icon={ faMagnifyingGlass }></FontAwesomeIcon>
-                  <input 
-                    type="text" 
-                    placeholder='Search' 
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                  />
+                <div className="internoBuscador">
+                    <div className='buscador'>
+                        <FontAwesomeIcon className='iconLupa' icon={faMagnifyingGlass}></FontAwesomeIcon>
+                        <input
+                            type="text"
+                            placeholder='Search'
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                        />
+                    </div>
                 </div>
-              </div>
             </div>
             <div className="contenedorBuscadorDesktop">
-              <div className="internoBuscador">
-                <div className="contenedorTituloDesktop">
-                  <h3>Proyectos</h3>
+                <div className="internoBuscador">
+                    <div className="contenedorTituloDesktop">
+                        <h3>Proyectos</h3>
+                    </div>
+                    <div className='buscador'>
+                        <FontAwesomeIcon className='iconLupa' icon={faMagnifyingGlass}></FontAwesomeIcon>
+                        <input
+                            type="text"
+                            placeholder='Search'
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                        />
+                    </div>
                 </div>
-                <div className='buscador'>
-                  <FontAwesomeIcon className='iconLupa' icon={ faMagnifyingGlass }></FontAwesomeIcon>
-                  <input 
-                    type="text" 
-                    placeholder='Search' 
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                  />
-                </div>
-              </div>
             </div>
             <div className="contenedorFiltro">
-              <div className="internoFiltro">
-              <button onClick={() => handleFilterChange('All')}>All</button>
-                <button onClick={() => handleFilterChange('Frontend')}>Front End</button>
-                <button onClick={() => handleFilterChange('Fullstack')}>Full Stack</button>
-              </div>
+                <div className="internoFiltro">
+                    <button onClick={() => handleFilterChange('All')}>All</button>
+                    <button onClick={() => handleFilterChange('Frontend')}>Front End</button>
+                    <button onClick={() => handleFilterChange('Fullstack')}>Full Stack</button>
+                </div>
             </div>
             <div className="contenedorProyects">
-            {arrayProjectsPage.filter(project => {
-              // Filtra por título y por filtro seleccionado
-              if (
-                (searchTerm === '' || project.title.toLowerCase().includes(searchTerm.toLowerCase())) &&
-                (filter === 'All' || project.stack.toLowerCase() === filter.toLowerCase())
-              ) {
-                return true;
-              }
-              return false;
-            }).map((project, index) => (
-              <ProyectCard
-                key={index}
-                id={project.id} 
-                title={project.title}
-                image={project.image}
-                deployLink={project.deployLink}
-                serverLink={project.serverLink}
-                repositoryLink={project.repositoryLink}
-                urlYoutube={project.urlYoutube}
-                technologies={project.technologies.map((tag, index) => {
-                  const TagComponent = tagComponents[tag];
-                  return TagComponent ? <TagComponent key={index} /> : null;
-                })}
-              />
-            ))}
-          </div>
+                {arrayProjectsPage
+                    .filter(project => {
+                        // Filtra por título y por filtro seleccionado
+                        if (
+                            (searchTerm === '' || project.title.toLowerCase().includes(searchTerm.toLowerCase())) &&
+                            (filter === 'All' || project.stack.toLowerCase() === filter.toLowerCase())
+                        ) {
+                            return true;
+                        }
+                        return false;
+                    })
+                    .slice(0, showMore ? arrayProjectsPage.length : projectsToShow) // Mostrar solo los primeros 'projectsToShow' proyectos
+                    .map((project, index) => (
+                        <ProyectCard
+                            key={index}
+                            id={project.id}
+                            title={project.title}
+                            image={project.image}
+                            deployLink={project.deployLink}
+                            serverLink={project.serverLink}
+                            repositoryLink={project.repositoryLink}
+                            urlYoutube={project.urlYoutube}
+                            technologies={project.technologies.map((tag, index) => {
+                                const TagComponent = tagComponents[tag];
+                                return TagComponent ? <TagComponent key={index} /> : null;
+                            })}
+                        />
+                    ))}
+            </div>
+            {!showMore && (
+                <div className="contenedorBtnVerMas">
+                    <button className="btnVerMas" onClick={handleShowMore}>Ver Más</button>
+                </div>
+            )}
         </article>
     );
 }
