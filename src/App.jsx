52e28e0f8a4cf2927efc54f698735c2a6dc978f4
Julian <julianfinelli97@gmail.com>
2025-04-22 23:18:32 -0300

    chore: construction

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,17 @@ import BlogSSR from './components/BlogSSR/BlogSSR.jsx';
 import BlogPackageJson from './components/BlogPackageJson/BlogPackageJson.jsx';
 import BlogMiddlewere from './components/BlogMiddlewere/BlogMiddlewere.jsx';
 import BlogQR from './components/BlogQR/BlogQR.jsx';
+import fotoRobot from "../public/foto_robot.jpg"
 
 function App() {
   return (
     <BrowserRouter>
-      <LenguajeProvider>
-        <OpenNavbarProvider>
-          <main className='contenedorMainApp'>
-            <Navbar/>
-            <Routes>
-              <Route path="/" element={<MyApp/>} />
-              <Route path="/proyects/:id" element={<ProyectDetail/>} />
-              <Route path="/distintostiposderenderizado" element={<BlogWeb/>} />
-              <Route path="/implementandomipropiossr" element={<BlogSSR/>} />
-              <Route path="/queeselpackagejson" element={<BlogPackageJson/>} />
-              <Route path="/quesonlosmiddlewere" element={<BlogMiddlewere/>} />
-              <Route path="/qrpdf" element={<BlogQR />} />
-              <Route path="/*" element={<Navigate to="/" replace/>} />
-            </Routes>
-          </main>
-        </OpenNavbarProvider>
-      </LenguajeProvider>
+      <div className='construccion'>
+        <h1>En construcción</h1>
+        <div className='fotoRobot'>
+          <img src={fotoRobot} alt='fotoRobot'></img>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
