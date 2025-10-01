32d6d87c1a6d744643312359784a682696d882f1
Julian <julianfinelli97@gmail.com>
2025-10-01 12:40:06 -0300

    chore: back to old portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,29 @@ import BlogSSR from './components/BlogSSR/BlogSSR.jsx';
 import BlogPackageJson from './components/BlogPackageJson/BlogPackageJson.jsx';
 import BlogMiddlewere from './components/BlogMiddlewere/BlogMiddlewere.jsx';
 import BlogQR from './components/BlogQR/BlogQR.jsx';
-import fotoRobot from "../public/foto_robot.jpg"
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='construccion'>
-        <h1>En construcción</h1>
-        <div className='fotoRobot'>
-          <img src={fotoRobot} alt='fotoRobot'></img>
-        </div>
-      </div>
+      <LenguajeProvider>
+        <OpenNavbarProvider>
+          <main className='contenedorMainApp'>
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<MyApp/>} />
+              <Route path="/proyects/:id" element={<ProyectDetail/>} />
+              <Route path="/distintostiposderenderizado" element={<BlogWeb/>} />
+              <Route path="/implementandomipropiossr" element={<BlogSSR/>} />
+              <Route path="/queeselpackagejson" element={<BlogPackageJson/>} />
+              <Route path="/quesonlosmiddlewere" element={<BlogMiddlewere/>} />
+              <Route path="/qrpdf" element={<BlogQR />} />
+              <Route path="/*" element={<Navigate to="/" replace/>} />
+            </Routes>
+          </main>
+        </OpenNavbarProvider>
+      </LenguajeProvider>
     </BrowserRouter>
   );
 }
-
-
 
 export default App;
